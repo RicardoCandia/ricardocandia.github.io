bf092a2da27c4611874872b7941a54a01dce1863
RicardoCandia <ricardo.candia@gmail.com>
2023-08-20 20:58:57 -0400

    Deploy

---
+++ stories/ModalWindow.stories.js
@@ -0,0 +1,12 @@
+import { ModalWindow } from "./ModalWindow";
+
+export default {
+    ModalWindow: 'Example/ModalWindow',
+    component: ModalWindow
+};
+
+export const Primary = {
+    args: {
+        text: 'Este es el componente primario'
+    }
+}
